ca21af33ab343618db8586231cdfa6b5fc4afa57
Ayush Gusain <ayugusain1482001@gmail.com>
2023-05-25 12:41:09 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -40,7 +40,14 @@ class App extends Component {
   render() {
     return (
       <div id="main">
-        {/* Do not remove the main div */}
+        <ol>
+        {
+        this.cityList.map((ele ,index)=>{
+          return<>
+          <li key={index}>{ele.name}</li>
+          </>
+        })}
+        </ol>
       </div>
     )
   }
